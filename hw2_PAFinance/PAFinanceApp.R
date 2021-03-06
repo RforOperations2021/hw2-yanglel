e0c461b2a50ef66ea33c930d2a17eea5bd4fa88e
Yang Le <54694621+YangLe-L@users.noreply.github.com>
2021-03-06 18:28:56 -0500

    Create chart and boxes for second page

--- hw2_PAFinance/PAFinanceApp.R
+++ hw2_PAFinance/PAFinanceApp.R
@@ -9,7 +9,11 @@ library(plotly)
 load("PA_M_2006_2018_full.RData")
 year_min <- min(PA_M_2006_2018_full$Reporting_Year)
 year_max <- max(PA_M_2006_2018_full$Reporting_Year)
+# get list of county names
 county <- unique(c(PA_M_2006_2018_full$county_name, PA_M_2006_2018_full$county_name_2a))
+# create date variable
+PA_M_2006_2018_full$date <- as.Date(
+    paste(PA_M_2006_2018_full$Reporting_Year, 1, 1, sep="-"))
 
 # Avoid plotly issues 
 pdf(NULL)
@@ -23,21 +27,27 @@ ui <- dashboardPage(
     dashboardHeader(
         title = "PA Municipalities Public Finance Data (2006-2018)",
         titleWidth = 500
-        ),
+    ),
     
     # create sidebar
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Surplus/Deficit", tabName = "SD", icon = icon("money-bill-wave")),
+            menuItem("Surplus/Deficit", 
+                     tabName = "SD", 
+                     icon = icon("money-bill-wave")),
             
-            menuItem("External Revenue Dependency", tabName = "Ext", icon = icon("landmark")),
+            menuItem("External Revenue Dependency", 
+                     tabName = "Ext", 
+                     icon = icon("landmark")),
             
-            menuItem("Debt", tabName = "Debt", icon = icon("credit-card")),
+            menuItem("Debt", 
+                     tabName = "Debt", 
+                     icon = icon("credit-card")),
             
             # create filters
             sliderInput(
                 inputId = "year",
-                label = "Max Year (Scatterplot & Bar Chart) & Year Range (Line Chart & Data Table)",
+                label = "Year",
                 min = year_min,
                 max = year_max,
                 step = 1,
@@ -71,78 +81,105 @@ ui <- dashboardPage(
                 selectize = T ,
                 multiple =  T
             ),
-
+            
             # download button
             downloadButton(
                 outputId = "download",
                 label = "Download Data Table"
             )
-
+            
         )
-        ),
+    ),
     
     # create body
     dashboardBody(
         tabItems(
-            # first tab
+            # first page
             tabItem(tabName = "SD",
                     h2("Is Revenue Enough To Cover Expenditure?"),
-   
-                            fluidRow(
-                                    valueBoxOutput("YearOfDeficit", width = 6 ),
-                                    valueBoxOutput("Numberofdeficits", width = 6),
-                                    valueBoxOutput("DeficitSize", width = 6),
-                                    valueBoxOutput("surplusSize", width = 6)
-                                    
-                                    ),
-                                
-                             fluidRow(box(
-                                 plotlyOutput("scatter"), 
-                                 width = 12, 
-                                 "Above line (y = x) means presence of surplus and below line means presence of deficit.",
-                                 br(),
-                                 "Outliers in both axes are removed to assist to visualization",
-                                 br(),
-                                 "Median is used due to large outliers.")
+                    fluidRow(
+                        valueBoxOutput("YearOfDeficit", width = 6 ),
+                        valueBoxOutput("Numberofdeficits", width = 6),
+                        valueBoxOutput("DeficitSize", width = 6),
+                        valueBoxOutput("surplusSize", width = 6)
+                    ),
+                    
+                    fluidRow(
+                        box(plotlyOutput("scatter"), 
+                            width = 12, 
+                            "Above line (y = x) means presence of surplus and below line means presence of deficit.",
+                            br(),
+                            "Outliers in both axes are removed to assist to visualization",
+                            br(),
+                            "Median is used due to large outliers.")
                     )
-                    ),
-        
+            ),
+            
+            # second page
             tabItem(tabName = "Ext",
-                    h2("Is The Municipality Too Dependent On Other Governments For Revenue?")
+                    h2("Is The Municipality Too Dependent On Other Governments For Revenue?"),
+                    fluidRow(
+                        infoBoxOutput("ExtPer", width = 6),
+                        infoBoxOutput("Extsize", width = 6)
                     ),
+                    
+                    fluidRow(
+                        box(plotlyOutput("line"), 
+                            width = 12, 
+                            "Fund balance is the accumulated financial resources that is still unused over the years.", 
+                            br(), 
+                            "Median is used due to large outliers."
+                    )
+            )
+            ),
             
             tabItem(tabName = "Debt",
                     h2("Is There Too Much Debt?")
-                    )
-        
-        
+            )
         )
     )
-    )
+)
 
 
-
-
-    
-
-# Define server logic required to draw a histogram
+# Define server logic-----------------------------
 server <- function(input, output) {
     
-    # reactive subsetting for scatterplot and bar chart
-    
+    # subset data -----------------------------------
+    # reactive subsetting for single year charts 
     PA_subset <- reactive({
         req(input$year)
         req(input$type)
         a <- PA_M_2006_2018_full %>% 
-            filter((Reporting_Year == max(input$year)) & (Municipality_Type %in% input$type))
+            filter((Reporting_Year == max(input$year)) & 
+                       (Municipality_Type %in% input$type))
+        
         if (length(input$county_1) > 0) {
             a <- a %>% 
-               filter((county_name %in% input$county_1) | (county_name_2a %in% input$county_1)) 
+                filter((county_name %in% input$county_1) | 
+                           (county_name_2a %in% input$county_1)) 
         }
         return(a)
     })
     
- 
+    # create reative subset for mutiple years data
+    PA_subset_time <- reactive({
+        req(input$year)
+        req(input$type) 
+        a <- PA_M_2006_2018_full %>% 
+            filter((Reporting_Year <= max(input$year)) &
+                       (Reporting_Year >= min(input$year)) &
+                       (Municipality_Type %in% input$type))
+        if (length(input$county_1) > 0) {
+            a <- a %>% 
+                filter((county_name %in% input$county_1) | 
+                           (county_name_2a %in% input$county_1)) 
+        }
+        return(a)
+    })  
+    
+    
+    
+    # 1st page: Surplus/deficit------------------------------------
     # Create scatterplot
     output$scatter <- renderPlotly({
         # identify outliers
@@ -150,46 +187,107 @@ server <- function(input, output) {
         outliers_y <- (boxplot(PA_subset()$Revenues_Per_Capita))$out
         
         ggplotly(
-        PA_subset() %>%
-            filter(!(Expenditures_Per_Capita %in% outliers_x)) %>%
-            filter(!(Revenues_Per_Capita %in% outliers_y)) %>%
-            ggplot(aes(x = Expenditures_Per_Capita, y = Revenues_Per_Capita, text = paste(full_municipal_name,"\n", "RevPC:", Revenues_Per_Capita, "\n", "ExpPC:", Expenditures_Per_Capita) )) +
-            geom_point(aes(color = Municipality_Type)) +
-            geom_abline(intercept =0 , slope = 1, size = 1, color = "red",
-                        linetype = "dashed") +
-            geom_text(aes(x=150, label="Deficit Zone", y= 0), colour= "#D55E00")+
-            geom_text(aes(x=150, label="Surplus Zone", y=500), colour= "#0072B2") +
-            xlab("Expenditure per Capita($)") + ylab("Revenue Per Capita($)") +
-            labs(title = paste("Revenue Per Capita vs Expenditure per Capita",
-                "In",max(input$year))) + 
-            scale_color_discrete(name = "Municipality Type"),
-        tooltip = "text"
+            PA_subset() %>%
+                filter(!(Expenditures_Per_Capita %in% outliers_x)) %>%
+                filter(!(Revenues_Per_Capita %in% outliers_y)) %>%
+                ggplot(aes(x = Expenditures_Per_Capita, 
+                           y = Revenues_Per_Capita, 
+                           text = paste(full_municipal_name,"\n", "RevPC:",
+                                        Revenues_Per_Capita, "\n", "ExpPC:", 
+                                        Expenditures_Per_Capita))) +
+                geom_point(aes(color = Municipality_Type)) +
+                geom_abline(intercept =0 , slope = 1, size = 1, color = "red",
+                            linetype = "dashed") +
+                geom_text(aes(x=150, label="Deficit Zone", y= 0), 
+                          colour= "#D55E00")+
+                geom_text(aes(x=150, label="Surplus Zone", y=500), 
+                          colour= "#0072B2") +
+                xlab("Expenditure per Capita($)") + 
+                ylab("Revenue Per Capita($)") +
+                labs(title = 
+                         paste("Revenue Per Capita vs Expenditure per Capita",
+                               "In",max(input$year))) + 
+                scale_color_discrete(name = "Municipality Type"),
+            tooltip = "text"
         )
-     })
+    })
     
-    # value boxes for for surplus/deficit tab
+    # value boxes for surplus/deficit tab ----------------------
     output$YearOfDeficit <- renderValueBox({
-        valueBox(round(mean(PA_subset()$no_of_deficit_year, na.rm = T),0), paste("Average Years of Deficits As Of", max(input$year) , "(out of", max(input$year) - year_min, ")"), icon = icon("calendar-minus"), color = "purple")
-        })
+        valueBox(round(mean(PA_subset()$no_of_deficit_year, na.rm = T),0), 
+                 paste("Average Years of Deficits As Of", max(input$year) , "(out of", max(input$year) - year_min, ")"), 
+                 icon = icon("calendar-minus"), 
+                 color = "purple")
+    })
+    
+    output$Numberofdeficits <- renderValueBox({
+        valueBox(round(sum(PA_subset()$deficit, na.rm = T),0), 
+                 paste("Municipality Facing Deficits In", max(input$year) , "(out of", nrow(PA_subset()), ")"), 
+                 icon = icon("city"), 
+                 color = "blue")
+    })
+    
+    output$DeficitSize <- renderValueBox({
+        a <- PA_subset() %>% 
+            filter(deficit == 1)
         
-        output$Numberofdeficits <- renderValueBox({
-            valueBox(round(sum(PA_subset()$deficit, na.rm = T),0), paste("Municipality Facing Deficits In", max(input$year) , "(out of", nrow(PA_subset()), ")"), icon = icon("city"), color = "blue")
-        })
+        valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), 
+                 paste("Median Deficit Size In", max(input$year)), 
+                 icon = icon("minus-circle"), 
+                 color = "orange")
+    })
+    
+    output$surplusSize <- renderValueBox({
+        a <- PA_subset() %>% 
+            filter(deficit == 0)
         
-        output$DeficitSize <- renderValueBox({
-           a <- PA_subset() %>% 
-               filter(deficit == 1)
-            valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), paste("Median Deficit Size In", max(input$year)) , icon = icon("minus-circle"), color = "orange")
-        })
-        
-        output$surplusSize <- renderValueBox({
-            a <- PA_subset() %>% 
-                filter(deficit == 0)
-            valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), paste("Median Surplus Size In", max(input$year)) , icon = icon("plus-circle"), color = "green")
-        })
-        
-        
+        valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), 
+                 paste("Median Surplus Size In", max(input$year)), 
+                 icon = icon("plus-circle"), 
+                 color = "green")
+    })
+    
+    # Second page: External revenue -----------------------
+    
+    # charts for second page------------------------------
+    # line charts
+    
+    output$line <- renderPlotly({
+                # create table of median
+                med_subset_time_1 <- PA_subset_time() %>%
+                        group_by(Municipality_Type, date) %>%
+                        summarize(median =
+                                median(ext_revenue_over_revenue, na.rm = T)*100)
 
+    ggplotly(
+            ggplot(data = med_subset_time_1, aes(x = date, y = median)) +
+                    geom_line(aes(color = Municipality_Type)) +
+                geom_point() +
+                    scale_x_date(date_labels = "%Y", date_breaks = "3 years") +
+                    xlab("Year") +
+                    ylab("Share of External Revenue(%)") +
+                    labs(title = paste(
+                    "Median Revenue Share of Intergovernmental Revenue From",
+                    min(input$year), "To", max(input$year))),
+                    tooltip = c("x", "y")
+     )
+        })
+
+    
+    # info boxes for second page -------------------------------
+    output$ExtPer <- renderInfoBox({
+        infoBox(paste("Median Intergovernmental Revenue Per Capita In", max(input$year)),
+                paste("$", (scales::comma_format()(round(median(PA_subset()$intergovernmental_per_capita, na.rm = T),0)))),
+                icon = icon("external-link-alt"), 
+                color = "purple")
+    })
+    
+    output$Extsize <- renderInfoBox({
+        infoBox(paste("Median Revenue Share of Intergovernmental Revenue In", max(input$year)), 
+                paste((round((median(PA_subset()$ext_revenue_over_revenue, na.rm = T))*100,0)), "%"),
+                icon = icon("hands-helping"), 
+                color = "blue")
+    })
     
 }
     
