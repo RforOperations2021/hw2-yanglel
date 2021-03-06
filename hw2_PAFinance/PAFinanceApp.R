4e9e4e527eacbd815fad5eff9ae9ca3bdb42d4f2
Yang Le <54694621+YangLe-L@users.noreply.github.com>
2021-03-06 14:29:09 -0500

    Add first page

--- hw2_PAFinance/PAFinanceApp.R
+++ hw2_PAFinance/PAFinanceApp.R
@@ -7,13 +7,9 @@ library(plotly)
 
 # load data
 load("PA_M_2006_2018_full.RData")
-pop_max <- max(PA_M_2006_2018_full$Population)
-PA_M_2006_2018_full$deficit <- factor(PA_M_2006_2018_full$deficit, label = c("No", "Yes"))
 year_min <- min(PA_M_2006_2018_full$Reporting_Year)
 year_max <- max(PA_M_2006_2018_full$Reporting_Year)
-county <- unique(c(PA_M_2006_2018_full$county_name, PA_M_2006_2018_full$county_name_2))
-county <- county[!is.na(county)]
-
+county <- unique(c(PA_M_2006_2018_full$county_name, PA_M_2006_2018_full$county_name_2a))
 
 # Avoid plotly issues 
 pdf(NULL)
@@ -69,19 +65,19 @@ ui <- dashboardPage(
             
             # create county comparison filters
             selectInput(
-                inputId = "county",
+                inputId = "county_1",
                 label = "County (Type to search, backspace to delete)",
                 choices = county,
                 selectize = T ,
                 multiple =  T
             ),
-            
+
             # download button
             downloadButton(
-                outputId = "download", 
+                outputId = "download",
                 label = "Download Data Table"
             )
-            
+
         )
         ),
     
@@ -91,13 +87,23 @@ ui <- dashboardPage(
             # first tab
             tabItem(tabName = "SD",
                     h2("Is Revenue Enough To Cover Expenditure?"),
-                    
-                    fluidRow(
-                        tabBox(
-                            width = 12,
-                            tabPanel("Tab1", "First tab content"),
-                            tabPanel("Tab2", "Tab content 2")
-                        )
+   
+                            fluidRow(
+                                    valueBoxOutput("YearOfDeficit", width = 6 ),
+                                    valueBoxOutput("Numberofdeficits", width = 6),
+                                    valueBoxOutput("DeficitSize", width = 6),
+                                    valueBoxOutput("surplusSize", width = 6)
+                                    
+                                    ),
+                                
+                             fluidRow(box(
+                                 plotlyOutput("scatter"), 
+                                 width = 12, 
+                                 "Above line (y = x) means presence of surplus and below line means presence of deficit.",
+                                 br(),
+                                 "Outliers in both axes are removed to assist to visualization",
+                                 br(),
+                                 "Median is used due to large outliers.")
                     )
                     ),
         
@@ -108,50 +114,86 @@ ui <- dashboardPage(
             tabItem(tabName = "Debt",
                     h2("Is There Too Much Debt?")
                     )
+        
+        
         )
-        
-        
-        
     )
-) 
+    )
+
+
+
+
     
-    
-    
-    
-#     fluidPage(
-# 
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    # reactive subsetting for scatterplot and bar chart
+    
+    PA_subset <- reactive({
+        req(input$year)
+        req(input$type)
+        a <- PA_M_2006_2018_full %>% 
+            filter((Reporting_Year == max(input$year)) & (Municipality_Type %in% input$type))
+        if (length(input$county_1) > 0) {
+            a <- a %>% 
+               filter((county_name %in% input$county_1) | (county_name_2a %in% input$county_1)) 
+        }
+        return(a)
+    })
+    
+ 
+    # Create scatterplot
+    output$scatter <- renderPlotly({
+        # identify outliers
+        outliers_x <- (boxplot(PA_subset()$Expenditures_Per_Capita))$out
+        outliers_y <- (boxplot(PA_subset()$Revenues_Per_Capita))$out
+        
+        ggplotly(
+        PA_subset() %>%
+            filter(!(Expenditures_Per_Capita %in% outliers_x)) %>%
+            filter(!(Revenues_Per_Capita %in% outliers_y)) %>%
+            ggplot(aes(x = Expenditures_Per_Capita, y = Revenues_Per_Capita, text = paste(full_municipal_name,"\n", "RevPC:", Revenues_Per_Capita, "\n", "ExpPC:", Expenditures_Per_Capita) )) +
+            geom_point(aes(color = Municipality_Type)) +
+            geom_abline(intercept =0 , slope = 1, size = 1, color = "red",
+                        linetype = "dashed") +
+            geom_text(aes(x=150, label="Deficit Zone", y= 0), colour= "#D55E00")+
+            geom_text(aes(x=150, label="Surplus Zone", y=500), colour= "#0072B2") +
+            xlab("Expenditure per Capita($)") + ylab("Revenue Per Capita($)") +
+            labs(title = paste("Revenue Per Capita vs Expenditure per Capita",
+                "In",max(input$year))) + 
+            scale_color_discrete(name = "Municipality Type"),
+        tooltip = "text"
+        )
+     })
+    
+    # value boxes for for surplus/deficit tab
+    output$YearOfDeficit <- renderValueBox({
+        valueBox(round(mean(PA_subset()$no_of_deficit_year, na.rm = T),0), paste("Average Years of Deficits As Of", max(input$year) , "(out of", max(input$year) - year_min, ")"), icon = icon("calendar-minus"), color = "purple")
+        })
+        
+        output$Numberofdeficits <- renderValueBox({
+            valueBox(round(sum(PA_subset()$deficit, na.rm = T),0), paste("Municipality Facing Deficits In", max(input$year) , "(out of", nrow(PA_subset()), ")"), icon = icon("city"), color = "blue")
+        })
+        
+        output$DeficitSize <- renderValueBox({
+           a <- PA_subset() %>% 
+               filter(deficit == 1)
+            valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), paste("Median Deficit Size In", max(input$year)) , icon = icon("minus-circle"), color = "orange")
+        })
+        
+        output$surplusSize <- renderValueBox({
+            a <- PA_subset() %>% 
+                filter(deficit == 0)
+            valueBox(paste("$", (scales::comma_format()(round(median(a$Revenues_Over_Expenditures, na.rm = T),0)))), paste("Median Surplus Size In", max(input$year)) , icon = icon("plus-circle"), color = "green")
+        })
+        
+        
 
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # 
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
+    
 }
+    
+    
 
 # Run the application 
 shinyApp(ui = ui, server = server)
